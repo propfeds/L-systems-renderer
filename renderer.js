bd7d37368aa8da6f015bdc43eb0061534ceaca64
Minh <propfeds@gmail.com>
2022-11-06 18:55:10 +0900

    Updated theory description

--- renderer.js
+++ renderer.js
@@ -10,7 +10,7 @@ import { Color } from '../api/ui/properties/Color';
 
 var id = 'L_systems_renderer';
 var name = 'L-systems Renderer';
-var description = 'A renderer of L-systems.\n\nFeatures:\n- Supports a whole army of production rules!\n- Stochastic (randomised) systems\n- Two camera modes: fixed (scaled) and cursor-focused\n- Stroke options\n\nWarning: As of 0.15, a theory reset is required due to internal state format changes.';
+var description = 'An educational tool that lets you draw various fractal figures and plants.\n\nFeatures:\n- Supports a whole army of production rules!\n- Stochastic (randomised) systems\n- Two camera modes: fixed (scaled) and cursor-focused\n- Stroke options\n\nWarning: As of 0.15, a theory reset is required due to internal state format changes.';
 var authors = 'propfeds#5988';
 var version = 'v0.16';
 
